1c6288ee7eb600c278c74ed0768395a1cb271ffb
itsdiegocosta <102886728+itsdiegocosta@users.noreply.github.com>
2022-06-02 23:07:04 -0300

    Update segundo.java

--- segundo.java
+++ segundo.java
@@ -1,12 +1,8 @@
-package Fundamentos;
-
-public class PrimeiroPrograma {
-
-	public static void main(String[] args) {
-
-		var a = 1;
-		var b = 2;
-		var c = -8;
+class Main {
+  public static void main(String[] args) {
+    		var a = digite um numero;
+		var b = digite um numero;
+		var c = digite um numero;
 		var delta = Math.pow(b, 2) - 4 * a * c;
 		var deltar = Math.pow(delta, 0.5);
 
@@ -16,6 +12,5 @@ public class PrimeiroPrograma {
 		System.out.println(x1);
 		System.out.println(x2);
 		System.out.println(delta);
-
-	}
+  }
 }
